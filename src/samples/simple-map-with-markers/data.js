9dd0767232e9dd31a18793a2a974e751a0036017
JoscelynJean <joscelyn.jean@ville.montreal.qc.ca>
2017-08-22 15:48:19 -0400

    Setup + simple map

---
+++ src/samples/simple-map-with-markers/data.js
@@ -0,0 +1,38 @@
+var D = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.56951713562012,
+          45.49626289450576
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.56624484062195,
+          45.498233215234656
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.55365991592407,
+          45.4958191868156
+        ]
+      }
+    }
+  ]
+};
